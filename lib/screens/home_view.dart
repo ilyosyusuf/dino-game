2c78a90c9192a3dd17187f288e5d706b39880f7f
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-06-15 14:26:00 +0500

    main works have been completed

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -9,9 +11,9 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   AnimationController? dinosaurController;
+  AnimationController? cactusController;
 
   @override
   void initState() {
@@ -21,16 +23,82 @@ class _HomeViewState extends State<HomeView>
     dinosaurController = AnimationController(
         vsync: this,
         lowerBound: 410,
-        upperBound: 500,
-        duration: Duration(milliseconds: 800));
+        upperBound: 490,
+        duration: Duration(milliseconds: 700));
+
+    cactusController = AnimationController(
+        vsync: this,
+        lowerBound: 0,
+        upperBound: 460,
+        duration: Duration(seconds: 4));
+    cactusController!.forward();
+    cactusController!.addListener(() {
+      debugPrint(cactusController!.value.toString());
+      if (dinosaurController!.value > 408.0 &&
+          dinosaurController!.value < 450.0 &&
+          cactusController!.value > 288.5 &&
+          cactusController!.value < 360.0) {
+        print("kichik");
+        cactusController!.stop();
+        dinosaurController!.stop();
+
+      }
+      setState(() {});
+    });
+
+    cactusController!.addStatusListener((status) {
+      debugPrint("Status: $status");
+
+      if (status == AnimationStatus.completed) {
+        cactusController!.repeat();
+        setState(() {});
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: InkWell(
-        
+        onTap: () {
+          dinosaurController!.forward();
+          dinosaurController!.addListener(() {
+            debugPrint(dinosaurController!.value.toString());
+            setState(() {});
+          });
+
+          dinosaurController!.addStatusListener((status) {
+            debugPrint("Status: $status");
+            if (status == AnimationStatus.completed) {
+              dinosaurController!.reverse();
+            }
+          });
+
+          setState(() {});
+        },
+        child: Stack(
+          children: [
+            Center(child: Image.asset('assets/images/ground.gif')),
+            Positioned(
+              left: 20,
+              bottom: dinosaurController!.value,
+              child: Image.asset(
+                'assets/images/dinosaur.png',
+                width: 50,
+              ),
+            ),
+            Positioned(
+              right: cactusController!.value,
+              bottom: 410,
+              child: Image.asset(
+                'assets/images/cactus.png',
+                width: 50,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
 }
